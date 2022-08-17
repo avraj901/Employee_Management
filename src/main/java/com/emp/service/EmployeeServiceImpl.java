f64da95fce9678dcfce354c774efd4a408c6d9eb
unknown <cogjava3115@iiht.tech>
2022-08-17 14:10:01 +0530

    Added CRUD operation

---
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -0,0 +1,60 @@
+package com.emp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emp.exception.ResourceNotFoundException;
+import com.emp.model.Employee;
+
+@Service
+public class EmployeeServiceImpl implements IEmployeeService {
+
+	@Autowired
+	IEmployeeRepository employeeRepository;
+
+	@Override
+	public Integer saveEmployee(Employee employee) {
+		Employee saveEmployee = employeeRepository.save(employee);
+		return saveEmployee.getId();
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Optional<Employee> getEmployee(Integer id) {
+		return employeeRepository.findById(id);
+	}
+
+	@Override
+	public void deleteEmployee(Integer id) {
+		employeeRepository.deleteById(id);
+	}
+
+	@Override
+	public void deleteAllEmployee() {
+		employeeRepository.deleteAll();
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee, Integer id) {
+		
+		//We will check whether the employee exists with the given id or not 
+		
+		Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Employee", "id", id));
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setLastName(employee.getLastName());
+		existingEmployee.setEmailId(employee.getEmailId());
+		
+		//We will update the value and then save the updated value
+		
+		employeeRepository.save(existingEmployee);
+		return existingEmployee;
+	}
+}
